c368e121b61fea7f0cdf3b147916a02cae039ce1
Sagar Chavan <sagar.c.03@gmail.com>
2021-11-02 19:18:43 -0400

    sidebar-nvim config

--- init.lua
+++ init.lua
@@ -73,3 +73,23 @@ catppuccino.setup({
 }) ]]
 
 require("surround").setup({ mappings_style = "sandwich" })
+
+-- TODO: Something
+
+require("sidebar-nvim").setup({
+	disable_default_keybindings = 0,
+	bindings = nil,
+	open = false,
+	side = "right",
+	initial_width = 15,
+	update_interval = 1000,
+	sections = { "datetime", "git-status", "lsp-diagnostics", "todos" },
+	section_separator = "-----",
+	containers = {
+		attach_shell = "/bin/sh",
+		show_all = true,
+		interval = 5000,
+	},
+	datetime = { format = "%a %b %d, %H:%M", clocks = { { name = "local" } } },
+	todos = { ignored_paths = { "~" } },
+})
