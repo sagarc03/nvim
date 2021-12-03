8cce2af677a113bbdb7ef03b563a853ea7828279
Sagar Chavann <sagar.c.03@gmail.com>
2021-12-03 17:35:23 -0500

    updated bufferline to make it more natural

--- lua/bufferlineconfig.lua
+++ lua/bufferlineconfig.lua
@@ -57,9 +57,9 @@ require("bufferline").setup({
 		--   persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
 		--   -- can also be a table containing 2 custom separators
 		--   -- [focused and unfocused]. eg: { '|', '|' }
-		separator_style = "slant", -- "slant" | "thick" | "thin" | { 'any', 'any' },
-		--   enforce_regular_tabs = false | true,
-		--   always_show_bufferline = true | false,
+		separator_style = "thin", -- "slant" | "thick" | "thin" | { 'any', 'any' },
+		enforce_regular_tabs = true, -- false | true,
+		always_show_bufferline = false, --true | false,
 		--   sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
 		--     -- add custom logic
 		--     return buffer_a.modified > buffer_b.modified
