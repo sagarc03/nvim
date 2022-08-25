69be90ab8eeb8890ed48f22783f35a1b306b9e4f
Sagar Chavan <sagar.c.03@gmail.com>
2022-08-25 11:34:41 -0400

    change color scheme to kanagawa

--- init.lua
+++ init.lua
@@ -20,9 +20,25 @@ require("git")
 require("zen")
 
 -- Theme
-vim.g.sonokai_style = "andromeda"
-vim.g.sonokai_better_performance = 1
-vim.cmd("colorscheme sonokai")
+require("kanagawa").setup({
+	undercurl = true, -- enable undercurls
+	commentStyle = { italic = true },
+	functionStyle = { bold = true },
+	keywordStyle = { italic = true, bold = true },
+	statementStyle = { bold = true },
+	typeStyle = {},
+	variablebuiltinStyle = { italic = true },
+	specialReturn = true, -- special highlight for the return keyword
+	specialException = true, -- special highlight for exception handling keywords
+	transparent = false, -- do not set background color
+	dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+	globalStatus = false, -- adjust window separators highlight for laststatus=3
+	terminalColors = true, -- define vim.g.terminal_color_{0,17}
+	colors = {},
+	overrides = {},
+	theme = "default", -- Load "default" theme or the experimental "light" theme
+})
+vim.cmd("colorscheme kanagawa")
 
 require("Comment").setup()
 require("mini.surround").setup({
